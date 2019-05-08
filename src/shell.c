132ff4b2d5c83512f5a8a22e1d2afee4b6c0f8b9
Chan Woo Park <14cpark@gmail.com>
2019-05-08 13:25:27 -0700

    initial project tar

---
+++ src/shell.c
@@ -0,0 +1,98 @@
+#include "shell_util.h"
+#include "linkedList.h"
+#include "helpers.h"
+
+// Library Includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+	int i; //loop counter
+	char *args[MAX_TOKENS + 1];
+	int exec_result;
+	int exit_status;
+	pid_t pid;
+	pid_t wait_result;
+    List_t bg_list;
+
+    //Initialize the linked list
+    bg_list.head = NULL;
+    bg_list.length = 0;
+    bg_list.comparator = NULL;  // Don't forget to initialize this to your comparator!!!
+
+	// Setup segmentation fault handler
+	if(signal(SIGSEGV, sigsegv_handler) == SIG_ERR)
+	{
+		perror("Failed to set signal handler");
+		exit(-1);
+	}
+
+	while(1) {
+		// DO NOT MODIFY buffer
+		// The buffer is dynamically allocated, we need to free it at the end of the loop
+		char * const buffer = NULL;
+		size_t buf_size = 0;
+
+		// Print the shell prompt
+		display_shell_prompt();
+		
+		// Read line from STDIN
+		ssize_t nbytes = getline((char **)&buffer, &buf_size, stdin);
+
+		// No more input from STDIN, free buffer and terminate
+		if(nbytes == -1) {
+			free(buffer);
+			break;
+		}
+
+		// Remove newline character from buffer, if it's there
+		if(buffer[nbytes - 1] == '\n')
+			buffer[nbytes- 1] = '\0';
+
+		// Handling empty strings
+		if(strcmp(buffer, "") == 0) {
+			free(buffer);
+			continue;
+		}
+		
+		// Parsing input string into a sequence of tokens
+		size_t numTokens;
+		*args = NULL;
+		numTokens = tokenizer(buffer, args);
+
+		if(strcmp(args[0],"exit") == 0) {
+			// Terminating the shell
+			free(buffer);
+			return 0;
+		}
+
+		pid = fork();   //In need of error handling......
+
+		if (pid == 0){ //If zero, then it's the child process
+			exec_result = execvp(args[0], &args[0]);
+			if(exec_result == -1){ //Error checking
+				printf(EXEC_ERR, args[0]);
+				exit(EXIT_FAILURE);
+			}
+		    exit(EXIT_SUCCESS);
+		}
+		 else{ // Parent Process
+			wait_result = waitpid(pid, &exit_status, 0);
+			if(wait_result == -1){
+				printf(WAIT_ERR);
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		// Free the buffer allocated from getline
+		free(buffer);
+	}
+	return 0;
+}
+
